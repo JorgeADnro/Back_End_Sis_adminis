b5a628b89dde12c44305d7621c53085d14d2ecc5
JorgeADnro <106614143+JorgeADnro@users.noreply.github.com>
2023-02-25 13:50:23 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-
 const express = require('express');
 const mongoose = require("mongoose");
 require("dotenv").config();
-const userRouters = require('./routes/users');
+const userRouters = require('./src/routes/users');
 const app = express();
 const port = process.env.PORT || 9000;
 const cors = require("cors");
 
+
+// middleware
 app.use(cors());
 app.use(express.json());
 app.use('/api/', userRouters);
 
+
+// mongodb connection
 mongoose
     .connect(process.env.MONGODB_URI)
     .then(() => console.log('Connected to MongoDB Atlass'))
