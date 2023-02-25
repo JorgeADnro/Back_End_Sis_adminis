e0b9e2d7be3b4f40477ac98f3a253b7788ffd07d
JorgeADnro <106614143+JorgeADnro@users.noreply.github.com>
2023-02-25 13:49:43 -0600

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,20 +1,20 @@
 const express = require("express");
-const empleadoController = require('../controllers/empleadoController.js');
+const pacienteController = require('../controllers/pacienteController.js');
 const router = express.Router();
 
 //Create user
-router.post('/users', empleadoController.crearEmpleado);
+router.post('/users', pacienteController.crearPaciente);
 
 // get all users
-router.get('/users',empleadoController.obtenerEmpleado);
+router.get('/users',pacienteController.obtenerPacientes);
 
 // get a user
-router.get('/users/:id',empleadoController.obtenerEmpleado);
+router.get('/users/:id',pacienteController.obtenerPaciente);
 
 // update  a user
-router.put('/users/:id', empleadoController.actualizarEmpleado);
+router.put('/users/:id', pacienteController.actualizarPaciente);
 
 // delete a user
-router.delete('/users/:id', empleadoController.eliminarEmpleado);
+router.delete('/users/:id', pacienteController.eliminarPaciente);
 
 module.exports = router;
